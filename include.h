a7479c6e039e05f4a7554e6a472b66ec8b787a99
Dimpal273 <72816486+Dimpal273@users.noreply.github.com>
2022-04-15 23:23:56 -0700

    testing

---
+++ include.h
@@ -0,0 +1,12 @@
+#include "include.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <signal.h>
